6e1bc24d9eaed9f6b4d6e91ea115598559848392
louis <louis@lemoncloud.io>
2019-05-09 15:17:33 +0900

    chore: commit auto-fix test for husky 3rd

--- src/controllers/hello/index.ts
+++ src/controllers/hello/index.ts
@@ -4,7 +4,7 @@ const router: Router = Router();
 
 router.get('/hello', (_: Request, res: Response) => {
     res.json({
-        result: "Hello World!",
+        result: 'Hello World!!!',
     });
 });
 
